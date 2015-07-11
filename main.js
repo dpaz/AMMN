26c98d17b74a31f8dea1dd430898fb8b6d643507
Miguel Avila Monroy <soyer893@gmail.com>
2015-07-11 13:00:31 +0200

    Suma de Bamboo

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
   $('#tabs').tab();
+
+  var bambooCount = $('#bambooCounter');
+
+  $('#getBamboo').click(function() {
+    var nBamboo = parseInt(bambooCount.text());
+    bambooCount.text( nBamboo + 1);
+  });
 });
