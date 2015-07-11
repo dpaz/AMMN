2547bc65f20452482354300a48f330ad4d31530e
diegojnb <diegojnb@gmail.com>
2015-07-11 16:17:56 +0200

    modificado el boton de koala y sus efectos

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
   var nBamboo = 0;
   var bambooCount = $('#bambooCounter');
   var koalaBut = $('#recruitKoala');
+  var koalaCount = $('#koalaCounter');
+  var nKoala = 0;
 
   $('#getBamboo').click(function() {
     var nBamboo = parseInt(bambooCount.text());
@@ -12,4 +14,20 @@ $(document).ready(function() {
       koalaBut.attr('class', 'btn noselect');
     }
   });
+
+  $('#recruitKoala').click(function(){
+  	var nKoala = parseInt(koalaCount.text());
+  	var nBamboo = parseInt(bambooCount.text());
+
+    if(nBamboo >= 5){
+    	nKoala ++;
+	    nBamboo = nBamboo -5;
+	    bambooCount.text( nBamboo);
+	    koalaCount.text( nKoala);
+      	if (nBamboo < 5) {
+      		koalaBut.attr('class','btn noselect notYet');
+      	};
+    } 
+  });
+
 });
