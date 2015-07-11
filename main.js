15d6ef979173053bb236ed61835b380189738ad2
Miguel Avila Monroy <soyer893@gmail.com>
2015-07-11 15:32:24 +0200

    Koala boton css

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
   $('#tabs').tab();
-
+  var nBamboo = 0;
   var bambooCount = $('#bambooCounter');
+  var koalaBut = $('#recruitKoala');
 
   $('#getBamboo').click(function() {
     var nBamboo = parseInt(bambooCount.text());
-    bambooCount.text( nBamboo + 1);
+    nBamboo ++;
+    bambooCount.text( nBamboo);
+    if(nBamboo >= 5){
+      koalaBut.attr('class', 'btn noselect');
+    }
   });
 });
